2e45a9b95da00968d9416ba8fe72a26ecde14bc6
aaronlee1029 <aaronlee1029@gmail.com>
2020-07-12 02:21:22 -0400

    Add files via upload

---
+++ port_scanner.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env  python3
+import socket, threading
+
+
+def TCP_connect(ip, port_number, delay, output):
+    TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    TCPsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    TCPsock.settimeout(delay)
+    try:
+        TCPsock.connect((ip, port_number))
+        output[port_number] = 'Listening'
+    except:
+        output[port_number] = ''
+
+
+
+def scan_ports(host_ip, delay):
+
+    threads = []        # To run TCP_connect concurrently
+    output = {}         # For printing purposes
+
+    # Spawning threads to scan ports
+    for i in range(10000):
+        t = threading.Thread(target=TCP_connect, args=(host_ip, i, delay, output))
+        threads.append(t)
+
+    # Starting threads
+    for i in range(10000):
+        threads[i].start()
+
+    # Locking the main thread until all threads complete
+    for i in range(10000):
+        threads[i].join()
+
+    # Printing listening ports from small to large
+    for i in range(10000):
+        if output[i] == 'Listening':
+            print(str(i) + ': ' + output[i])
+
+
+
+def main():
+    ip = int("192.168.94.134")
+    host_ip = ip
+    delay = int(20)   
+    scan_ports(host_ip, delay)
+
+if __name__ == "__main__":
+    main()
