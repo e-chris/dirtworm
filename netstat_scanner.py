c5b59bfa21aaaf1c37857faffa2a036da32d1d62
aaronlee1029 <aaronlee1029@gmail.com>
2020-07-12 18:46:19 -0400

    Delete netstat_scanner.py

--- netstat_scanner.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-subprocess.call("netstat") #add any code you want to execute
-
-# Seomthing more dynamic could be
-# p = subprocess.Popen(["ping", "-c", "10", "ip/url"], stdout=subprocess.PIPE) => use to check other online systems or ping back to attacker to make sure victim's system is alive and well.
-# output, err = p.communicate()
-# print  output
